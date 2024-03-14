fdf2aeb64f477018c82a497efeedec4fc5c5f5de
Durian Rod <durianr1989@gmail.com>
2024-03-14 10:52:19 -0600

    se regresan cambios

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,14 +1,14 @@
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { EquiposProvider } from "./assets/context/EquiposContext.jsx"; // Importamos el proveedor del contexto
-import VerEquipos from "../src/assets/components/verequipos/VerEquipos.jsx";
-import AgregarEquipo from "../src/assets/components/agregarequipo/AgregarEquipo.jsx";
-import ActualizarEquipo from "../src/assets/components/actualizarequipos/ActualizarEquipo.jsx";
-import BusquedaEquipos from "../src/assets/components/busquedaequipos/BusquedaEquipos.jsx";
-import DatosCompletos from "../src/assets/components/datoscompletos/DatosCompletos.jsx";
-import Responsiva from "../src/assets/components/responsiva/Responsiva.jsx";
-import Header from "../src/assets/components/header/Header.jsx";
-import VistaAuditor from "../src/assets/components/vistaauditor//VistaAuditor.jsx";
+import VerEquipos from "./assets/components/verequipos/VerEquipos";
+import AgregarEquipo from "./assets/components/agregarequipo/AgregarEquipo";
+import ActualizarEquipo from "./assets/components/actualizarequipos/ActualizarEquipo";
+import BusquedaEquipos from "./assets/components/busquedaequipos/BusquedaEquipos";
+import DatosCompletos from "./assets/components/datoscompletos/DatosCompletos";
+import Responsiva from "./assets/components/responsiva/Responsiva";
+import Header from "./assets/components/header/Header";
+import VistaAuditor from "./assets/components/vistaauditor/VistaAuditor";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
