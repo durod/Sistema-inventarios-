bd17775ecb3a3f96d71cc75f8fae305993709e37
durod <durianr1989@mail.com>
2024-03-14 14:11:08 -0600

    prueba

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   return (
     <EquiposProvider> {/* Envolver la aplicación con el proveedor del contexto */}
       
-        <BrowserRouter>
+        <BrowserRouter basename="Sistema-inventarios-">
           <Header />
           <Routes>
             <Route path="/" element={<VerEquipos />} />
