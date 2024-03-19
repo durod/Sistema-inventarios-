0cf49d522066b1cfe8269899ce21065dd4faa807
Durian Rod <durianr1989@gmail.com>
2024-03-18 18:39:21 -0600

    SE AGREGAN MODELOS A MODELSDATA.JS

--- front/src/assets/modelsData.js
+++ front/src/assets/modelsData.js
@@ -1,46 +1,91 @@
 // Dell models
-export const dellModels = [
-    {
-      nombre: "latitude 3420",
+export const dellModels= [
+  {
+      nombre: "LATITUDE 3440",
+      procesador: "Intel Core i5-1335U",
+      almacenamiento: "256 GB"
+  },
+  {
+      nombre: "LATITUDE 3500",
+      procesador: "INTEL CORE i5-7200",
+      almacenamiento: "1 TB"
+  },
+  {
+      nombre: "LATITUDE 3540",
+      procesador: "Intel® Core™ i5-1135G7",
+      almacenamiento: "512 GB "
+  },
+  {
+      nombre: "LATITUDE 3590",
+      procesador: "Intel® Core™ i5-4590",
+      almacenamiento: "1 TB"
+  },
+  {
+      nombre: "Latitude 3400",
+      procesador: "Intel® Core™ i5-1135G7",
+      almacenamiento: "1 TB "
+  },
+  {
+      nombre: "latitude 3420 256",
       procesador: "Intel® Core™ i5-1135G7 de 11.ª generación",
-      almacenamiento: "SSD PCIe NVMe, M.2, clase 35 de 256 GB"
-    },
-    {
+      almacenamiento: "M.2, de 256 GB"
+  },
+  {
+      nombre: "latitude 3420 512",
+      procesador: "Intel® Core™ i5-1135G7 de 11.ª generación",
+      almacenamiento: "M.2, de 512 GB"
+  },
+  {
       nombre: "latitude 3421",
       procesador: "Intel® Core™ i5-1135G7 de 11.ª generación",
       almacenamiento: "512 GB"
-    },
-    {
-      nombre: "optiplex 3090",
-      procesador: "Intel® Core™ i5-1135G7 de 11.ª generación",
-      almacenamiento: "512 GB"
-    },
-    {
-      nombre: "latitude 3520T",
-      procesador: "11th Gen Intel(R) Core(TM) i5-1135G7",
-      almacenamiento: "512 GB"
-    },
-    {
-      nombre: "optiplex 9020",
-      procesador: "Core i5-4590T",
-      almacenamiento: "512 GB"
-    },
-    {
-      nombre: "optiplex 790",
-      procesador: "Intel Core i3-2120",
-      almacenamiento: "256 GB"
-    },
-    {
+  },
+  {
       nombre: "latitude 3520",
       procesador: "11th Gen Intel(R) Core(TM) i5-1135G7",
       almacenamiento: "512 GB"
-    },
-    {
+  },
+  {
+      nombre: "latitude 3520T",
+      procesador: "11th Gen Intel(R) Core(TM) i5-1135G7",
+      almacenamiento: "512 GB"
+  },
+  {
+      nombre: "OptiPlex 3080 Micro",
+      procesador: "Intel® Core™ i5-1135G7",
+      almacenamiento: "M.2 512 GB "
+  },
+  {
+      nombre: "OPTIPLEX 3040",
+      procesador: "Intel Core I5 2.40GHz 11gen",
+      almacenamiento: "1 TB"
+  },
+  {
       nombre: "optiplex 3080",
       procesador: "Intel® Core™ i5-10500T de 10.ª generación",
-      almacenamiento: "SSD M.2 512GB PCIe NVMe Class 35"
-    },
-  ];
+      almacenamiento: "M.2 512 GB "
+  },
+  {
+      nombre: "optiplex 3090",
+      procesador: "Intel® Core™ i5-1135G7 de 11.ª generación",
+      almacenamiento: "512 GB"
+  },
+  {
+      nombre: "OPTIPLEX 7050",
+      procesador: "Intel® Core™ i5-1135G7",
+      almacenamiento: "1 TB"
+  },
+  {
+      nombre: "optiplex 790",
+      procesador: "Intel Core i3-2120",
+      almacenamiento: "256 GB"
+  },
+  {
+      nombre: "optiplex 9020",
+      procesador: "Core i5-4590T",
+      almacenamiento: "512 GB"
+  }
+];
   
   // Apple models
   export const appleModels = [
@@ -54,6 +99,23 @@ export const dellModels = [
       procesador: "Intel Core i5 de 10.ª generación, 6 núcleos",
       almacenamiento: "SSD de 512 GB"
     },
+    {
+      nombre: "MAC MINI",
+      procesador: "APPLE M2",
+      almacenamiento: "512 GB"
+    },
+    {
+      nombre: "MAC STUDIO",
+      procesador: "APPLE M1 MAX",
+      almacenamiento: "512 GB"
+    },
+    {
+      nombre: "Mac Book Pro 16 inch",
+      procesador: "APPLE M1 PRO",
+      almacenamiento: "512 GB"
+    },
     // Agrega más modelos Apple según sea necesario
   ];
+
+  
   
