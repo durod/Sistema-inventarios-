2a3b9a72eda4afecd76af5cbcf705a3dba42e8b0
Durian Rod <durianr1989@gmail.com>
2024-03-18 22:54:11 -0600

    cambio en header

--- front/src/assets/components/header/Header.jsx
+++ front/src/assets/components/header/Header.jsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React from 'react'
 import "../header/estiloheader.css";
 
 function Header() {
   return (
     <header className="sticky-header">
-      <a href="/https://durod.github.io/Sistema-inventarios-">
+      
+ <a href="/">
         <img
           src="https://intranet.eleconomista.com.mx/img/logoeconomista.svg"
           alt="Logo de El Economista"
         />
       </a>
+      
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
